d4a454fd3b3e099c45bd4bbb3a7bdce8bda3edc8
jgruber@f5.com <jgruber@f5.com>
2025-05-23 08:35:41 -0500

    fixed sed typo

--- utils/make_provisioning_script.sh
+++ utils/make_provisioning_script.sh
@@ -21,17 +21,7 @@ echo "echo \$archive | base64 -d | tar -xz -C /" >> onboarding.sh
 echo "chmod +x '$host_destination_path/onboarding.sh'" >> onboarding.sh
 echo "nohup sh -c '$host_destination_path/onboarding.sh' >> /var/log/F5NetworksBIGIPOnboard.log" >> onboarding.sh
 
-#echo '#!/bin/bash' > extract.sh
-#echo "tar_marker=\$(sed -n '/^__TAR_BEGIN__\$/=' \$0)" >> extract.sh
-#echo 'tail -n +$((tar_marker+1)) $0 | tar -xz -C /' >> extract.sh
-#echo "chmod +x '$host_destination_path/onboarding.sh'" >> extract.sh
-#echo "bash '$host_destination_path/onboarding.sh' >> /var/log/F5NetworksBIGIPOnboard.log" >> extract.sh
-#echo 'exit 0'  >> extract.sh
-#echo '__TAR_BEGIN__'  >> extract.sh
-#cat extract.sh scripts.tar.gz > onboarding.sh
-
 rm -rf $(echo "$host_destination_path" | cut -d'/' -f2)
-#rm -rf ./extract.sh
 rm -rf ./scripts.tar.gz
 
 popd > /dev/null
